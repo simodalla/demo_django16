679a8d307cd1c0c1e67b89c5914c182ea2b18bcd
Simone Dalla <simodalla@gmail.com>
2014-02-08 13:38:49 +0100

    initial project

---
+++ demo_django16/demo_app/tests_aaa.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+class AAATest(TestCase):
+    def test_1(self):
+        self.assertEqual(1, 1)
+
+    def test_2(self):
+        self.assertEqual(1, 1)
